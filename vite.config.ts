c3bd7939ceddbfc636e18313cce50c22cbb2ce25
Maurice Conrad <conr.maur@gmail.com>
2024-06-20 20:02:15 +0200

    more external stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   //plugins: [vue()],
   build: {
-    outDir: './lib',
+    outDir: "./lib",
     lib: {
-      entry: path.resolve('src/index.ts'),
-      name: 'Expression Input',
-      fileName: (format) => `expression-input.${ format }.js`,
-      formats: ['es', 'umd'/*, 'cjs', 'amd', 'iife', 'system'*/],
+      entry: path.resolve("src/index.ts"),
+      name: "Expression Input",
+      fileName: (format) => `expression-input.${format}.js`,
+      formats: ["es", "umd" /*, 'cjs', 'amd', 'iife', 'system'*/],
     },
     rollupOptions: {
-      external: ['vue'],
+      external: ["vue", "lodash", "naive-ui", "@bluepic/core"],
       output: {
         sourcemap: false,
         // Provide global variables to use in the UMD build
         // Add external deps here
         globals: {
-          vue: 'Vue',
+          vue: "Vue",
+          lodash: "_",
         },
       },
     },
   },
-  plugins: [vue(), dts()]
-})
+  plugins: [vue(), dts()],
+});
